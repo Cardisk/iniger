c34298e2637413b3f672592a09df8c154cbbd3dd
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2023-05-22 21:21:13 +0200

    setting up things

---
+++ iniger.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Matteo Cardinaletti on 22/05/23.
+//
+
+#include "iniger.h"
+
+namespace ini {
+
+}
---
+++ iniger.h
@@ -0,0 +1,12 @@
+//
+// Created by Matteo Cardinaletti on 22/05/23.
+//
+
+#ifndef INIGER_H
+#define INIGER_H
+
+namespace ini {
+
+}
+
+#endif //INIGER_H
