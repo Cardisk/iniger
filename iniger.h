aa5518c013b43656e8b74138c3ed06c64aabf40f
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2023-06-17 16:50:52 +0200

    interface redesign better handling of temporary objects.

--- iniger.h
+++ iniger.h
@@ -8,34 +8,34 @@
 /*
  * ORIGINAL PARSER:
  *
- * ✅ (iniger.cpp:70) key symbol cannot contain "=" and ";" inside the Windows implementation.
+ * key symbol cannot contain "=" and ";" inside the Windows implementation.
  *
  * [symbol] defines a section. There is no section ending until the EOF or another section declaration.
  * (originally sections cannot be nested).
  *
- * ✅ (iniger.cpp:80) case-insensitive.
+ * case-insensitive.
  *
- * ✅ ";" at the beginning defines a comment that will be ignored.
+ * ";" at the beginning defines a comment that will be ignored.
  *
  * ✅ (just by using an unordered_map) the order of sections and properties is irrelevant.
  *
  * DERIVED FEATURES:
  *
- * ✅ (iniger.cpp:72) all the properties declared before any section are defined as "global".
+ * all the properties declared before any section are defined as "global".
  *
- * ✅ (iniger.cpp:80) ":" can be used instead of "=".
+ * ":" can be used instead of "=".
  *
  * section nesting is allowed with the usage of a "." notation.
  * relative nesting is allowed without specifying the upper section.
  *
- * ✅ "#" can be used instead of ";" to declare a comment.
+ * "#" can be used instead of ";" to declare a comment.
  *
- * ✅ ([abort] iniger.cpp:32) duplicate definition of the same property may cause an abort, override the older value or
+ * duplicate definition of the same property may cause an abort, override the older value or
  * define a multi-value.
  *
- * ✅ (if the section exists, add_property will append them) duplicate definition of a section will merge the properties.
+ * duplicate definition of a section will merge the properties.
  *
- * ✅ (iniger.cpp:96) quoted values are used to explicit define spaces inside values.
+ * quoted values are used to explicit define spaces inside values.
  */
 
 #include <iostream>
@@ -47,8 +47,7 @@
 namespace ini {
     class Section {
     public:
-        explicit Section() = default;
-        explicit Section(std::string sec_name) : sec_name(std::move(sec_name)) {}
+        explicit Section(std::string sec_name = "global") : sec_name(std::move(sec_name)) {}
 
         [[nodiscard]] bool props_empty() const {
             return props.empty();
@@ -82,7 +81,7 @@ namespace ini {
 
     class Object {
     public:
-        explicit Object(std::string file_path) : file_path(std::move(file_path)), global(Section("global")) {}
+        explicit Object(std::string file_path) : file_path(std::move(file_path)), global(Section()) {}
 
         [[nodiscard]] const std::string &get_file_path() const {
             return file_path;
@@ -136,13 +135,16 @@ namespace ini {
         std::string m_msg;
     };
 
-    bool add_property(Object &ini, const std::string &section_path, const std::string &key, const std::string &value);
+    bool add_property(Object &ini, std::string &key, std::string &value, std::string &section_path);
+    bool add_property(Object &ini, std::string &&key, std::string &&value, std::string &&section_path = "");
 
-    bool add_section(Object &ini, const std::string &new_section_name, const std::string &section_path);
+    bool add_section(Object &ini, std::string &new_section_name, std::string &section_path);
+    bool add_section(Object &ini, std::string &&new_section_name, std::string &&section_path = "");
 
-    Object read(const std::string &path);
+    Object read(std::string &path);
+    Object read(std::string &&path);
 
-    bool write(Object &ini, char key_val_separator);
+    bool write(Object &ini, const char key_val_separator);
 }
 
 #endif //INIGER_H
--- main.cpp
+++ main.cpp
@@ -3,43 +3,18 @@
 #include "iniger.h"
 
 int main() {
-    ini::Object ini("../test1.ini");
-
-    std::cout << std::boolalpha;
-    std::cout << "add_property result: "
-                << ini::add_property(ini, "global", "writing", "test")
-                << std::endl;
-    std::cout << "global empty: " << ini.get_global().props_empty() << std::endl;
-
-    std::cout << "-----------------\nGlobal:\n";
-    for (auto &i : ini.get_global().get_props()) {
+    std::unordered_map<std::string, int> map;
+    map.insert(std::make_pair("uno", 1));
+    map.insert(std::make_pair("due", 2));
+    for (auto &i : map) {
         std::cout << i.first << " -> " << i.second << std::endl;
     }
-    std::cout << "-----------------\n";
 
-    std::cout << "add_property result 2: "
-              << ini::add_property(ini, "FOO", "bar", "baz")
-              << std::endl;
-
-    std::cout << "-----------------\nSubSecs:\n";
-    for (auto &i : ini.get_global().get_subsecs()) {
-        std::cout << i.first << std::endl;
-        for (auto &j : i.second.get_props()) {
-            std::cout << "\t" << j.first << " -> " << j.second << std::endl;
-        }
+    // collisions are handled by aborting the insertion.
+    std::cout << map.insert(std::make_pair("uno", 10)).second << std::endl;
+    for (auto &i : map) {
+        std::cout << i.first << " -> " << i.second << std::endl;
     }
-    std::cout << "-----------------\n";
 
-//    for (auto &i : ini.get_global().get_props()) {
-//        std::cout << i.first << " -> " << i.second << std::endl;
-//    }
-//    std::cout << "-------------------\n";
-//    for (auto &i : ini.get_global().get_subsecs().at("FOO").get_props()) {
-//        std::cout << i.first << " -> " << i.second << std::endl;
-//    }
-
-    std::cout << "write result: " << ini::write(ini, ':');
-
-//    ini::read("../test.ini");
     return 0;
 }
