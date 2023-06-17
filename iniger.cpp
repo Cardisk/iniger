7e9c007a17dc5ae177fd2b6ffeb052dec06e2101
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2023-06-17 20:02:37 +0200

    implementing add_property and add_section

--- iniger.cpp
+++ iniger.cpp
@@ -37,18 +37,95 @@ std::string join(const std::vector<std::string> &v, const std::string &delim) {
 }
 
 bool ini::add_property(ini::Object &ini, std::string &key, std::string &value, std::string &section_path) {
-    return true;
+    if (key.empty() || value.empty()) return false;
+
+    // key symbol cannot contain "=" and ";" inside the Windows implementation.
+    if (key.contains('=') || key.contains(';')) {
+        std::cerr << "[ERROR]: key symbol cannot contain \"=\" and \";\" inside the Windows implementation\n";
+        return false;
+    }
+
+    ini::Section &sec = ini.get_global();
+
+    if (!section_path.empty()) {
+        auto path = string_split(section_path, ".");
+
+        for (auto &i : path) {
+            try {
+                sec = sec.get_subsecs().at(i);
+            } catch (std::out_of_range &e) {
+                // keep adding missing sections.
+                if (!ini::add_section(ini, i, section_path.substr(0, section_path.find(i)))) {
+                    std::cerr << "[ERROR]: could not create new section '" << i << "'\n";
+                    return false;
+                }
+                sec = sec.get_subsecs().at(i);
+            }
+        }
+    }
+
+    // case-insensitive.
+    return sec.get_props().insert(std::make_pair(to_lower(key), value)).second;
 }
 
 bool ini::add_property(ini::Object &ini, std::string &&key, std::string &&value, std::string &&section_path) {
     return ini::add_property(ini, key, value, section_path);
 }
 
+bool ini::add_property(ini::Object &ini, std::string &&key, std::string &value, std::string &&section_path) {
+    return ini::add_property(ini, key, value, section_path);
+}
+
+bool ini::add_property(ini::Object &ini, std::string &&key, std::string &&value, std::string &section_path) {
+    return ini::add_property(ini, key, value, section_path);
+}
+
 bool ini::add_section(ini::Object &ini, std::string &new_section_name, std::string &section_path) {
-    return true;
+    if (new_section_name.empty()) {
+        std::cerr << "[ERROR]: section name should not be empty\n";
+        return false;
+    }
+
+    ini::Section &sec = ini.get_global();
+    if (!section_path.empty()) {
+        auto path = string_split(section_path, ".");
+
+        for (auto &i : path) {
+            try {
+                sec = sec.get_subsecs().at(i);
+            } catch (std::out_of_range &e) {
+                // keep adding missing sections.
+                i = to_lower(i);
+                // rollbacks aren't handled at all.
+                // example: Foo          .Bar         .Baz
+                //          ^ existing    ^ missing    ^ missing
+                //                        ^ created    ^ failed
+                //                        ^ this should be deleted as well
+                // XXX: maybe this is useless because if I tried to create a specific
+                //      path is because I needed it.
+                if (!sec.get_subsecs().insert(
+                        std::make_pair(i, Section(i))).second) {
+                    std::cerr << "[ERROR]: could not create missing '" << i << "' section\n";
+                    return false;
+                }
+                sec = sec.get_subsecs().at(i);
+            }
+        }
+    }
+
+    new_section_name = to_lower(new_section_name);
+    return sec.get_subsecs().insert(std::make_pair(new_section_name, Section(new_section_name))).second;
 }
 
 bool ini::add_section(ini::Object &ini, std::string &&new_section_name, std::string &&section_path) {
+    return ini::add_section(ini, new_section_name, section_path);
+}
+
+bool ini::add_section(ini::Object &ini, std::string &new_section_name, std::string &&section_path) {
+    return ini::add_section(ini, new_section_name, section_path);
+}
+
+bool ini::add_section(ini::Object &ini, std::string &&new_section_name, std::string &section_path) {
     return ini::add_section(ini, new_section_name, section_path);
 }
 
